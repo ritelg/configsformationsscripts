c713019aece76508bd058eaa7e70847da1af0437
ritelg <ritelg@yahoo.fr>
2026-05-06 19:15:23 +0200

    workspace 4 pour packet tracer i3wm

---
+++ ansible/system-install/init-ansible.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+########################################################
+#
+# Description : Installation du systéme Debian avec ansible
+#
+# Auteur : Gaëtan Ritel
+#
+# Date : 08/02/25
+#
+########################################################
+
+RESET="\033[0m"
+GRAS="\033[1m"
+BLEU="\033[34m"
+ROUGE="\033[31m"
+PREFIX_STRING="###################"
+
+set -e 
+
+if [[ "$EUID" -ne 0 ]]
+then
+	echo -e "\n $GRAS $ROUGE Ce script doit être lancé en Root \n $RESET"
+	exit 1
+fi
+
+echo -e "\n $GRAS $BLEU $PREFIX_STRING Update du systéme $PREFIX_STRING $RESET \n"
+# apt update
+# apt upgrade -y
+
+echo -e "\n $GRAS $BLEU $PREFIX_STRING Installations des paquets de bases $PREFIX_STRING $RESET \n"
+apt install tig ranger zsh i3 rofi thunar compton tmux vim git curl wget python3-neovim polybar git-flow ripgrep gnupg rsync make ffmpeg htop fd-find silversearcher-ag exuberant-ctags libfuse2 maim xclip lshw jq
+apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+apt-get install -y  python3-pip python3-yaml python3-jinja2 python3-httplib2 python3-paramiko python3-pkg-resources ansible pipx nmap
+apt install libapr1 libaprutil1 libasound2 libglib2.0-0 libxcb-xinput0 -y
+apt install darktable -y
+
